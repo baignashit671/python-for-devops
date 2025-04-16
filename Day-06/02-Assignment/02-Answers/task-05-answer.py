9b9f72121ec7b9dc13066eaffbc84648a47eaf3e
baignashit671 <119288668+baignashit671@users.noreply.github.com>
2025-04-16 17:52:47 +0530

    Update task-05-answer.py

--- Day-06/02-Assignment/02-Answers/task-05-answer.py
+++ Day-06/02-Assignment/02-Answers/task-05-answer.py
@@ -1,3 +1,7 @@
+# Task 5: Bitwise Operators (Optional)
+# 1. If you are comfortable with bitwise operators, perform some bitwise operations on integer values and print the results. 
+# If not, you can skip this task.
+
 my_list = [1, 2, 3, 4, 5]
 
 # Identity operators
